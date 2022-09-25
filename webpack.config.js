f477ac359341a6faf5ae40f91405dae8c00ff34d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-25 20:16:58 -0300

    Building and serving with webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: { 
+                    presets: ["@babel/env"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", 
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '*',
+            '.js',
+            '.jsx'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: path.join(__dirname, 'public/'),
+        devMiddleware: {
+            publicPath: 'http://localhost:3000/dist/'
+        },
+        port: 3000,
+        hot: "only"
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+}
