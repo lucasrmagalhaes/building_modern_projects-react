62437df698fa6ddd60b800220f9d830c050c1b5e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-26 13:58:19 -0300

    Persisting the Redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 import App from './App.js';
 
@@ -9,10 +11,18 @@ import { store } from './store.js';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const configureStore = store;
+const persistor = persistStore(configureStore);
+
 root.render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
+        <Provider store={configureStore}>
+            <PersistGate 
+                persistor={persistor}
+                loading={<div>Loading...</div>}
+            >
+                <App />
+            </PersistGate>
         </Provider>
     </StrictMode>
 );
