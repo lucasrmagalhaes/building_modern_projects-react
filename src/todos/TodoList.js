07ed5c3010c262511e3ea025d5b4ca829afc010e
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-09-25 21:13:22 -0300

    Creating the todoListItem component

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
+import './TodoList.css';
 
 const TodoList = ({ todos }) => (
     <div className='list-wrapper'>
-        {todos.map(todo => <TodoListItem todo={todo} />)}
+        {todos.map(todo => 
+            <TodoListItem 
+                todo={todo} 
+            />
+        )}
     </div>
 );
 
